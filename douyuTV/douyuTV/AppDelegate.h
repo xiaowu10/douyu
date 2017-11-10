d2338b73716ab6994ac16adfb9210117410d4d35
xiaowu <1366353212@qq.com>
2017-11-11 00:01:22 +0800

    实验11.10

---
+++ douyuTV/douyuTV/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  douyuTV
+//
+//  Created by Mac on 17/11/10.
+//  Copyright © 2017年 Mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
